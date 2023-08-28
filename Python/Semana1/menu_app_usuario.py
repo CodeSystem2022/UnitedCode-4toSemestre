4734a8a367194b412aba73970a7253bad0dd7ed4
Katherinelop <69828273+Katherinelop@users.noreply.github.com>
2023-08-27 21:54:10 -0300

    menu_app_usuario.py
    agrego la opcion 3 y 4

--- Python/Semana1/menu_app_usuario.py
+++ Python/Semana1/menu_app_usuario.py
@@ -20,7 +20,21 @@ while opcion != 5:
         passwoord_var = input('Digite su contraseña: ')
         usuario = Usuario(username=username_var, password=passwoord_var)
         usuario_insertado = UsuarioDAO.insertar(usuario)
-        log.info(f'Usuario insertado: {usuario_insertado}')
+        log.info(f'Usuario insertado: {usuario_insertado}') 
+
+        elif opcion == 3:
+        id_usuario_var = int(input('Digite el id del usuario a modificar: '))
+        username_var = input('Digite el nombre del usuario a modificar: ')
+        password_var = input('Digite la contraseña del usuario a modificar: ')
+        usuario = Usuario(id_usuario=id_usuario_var, username=username_var, password=password_var)
+        usuario_actualizado = UsuarioDAO.actualizar(usuario)
+        log.info(f'Usuario actualizado: {usuario_actualizado}')
+    elif opcion == 4:
+        id_usuario_var = int(input('Digite el id del usuario a eliminar: '))
+        usuario = Usuario(id_usuario=id_usuario_var)
+        usuario_eliminado = UsuarioDAO.eliminar(usuario)
+        log.info(f'Usuario eliminado: {usuario_eliminado}')
+
     
 else:
     log.info('Salimos de la aplicacion, hasta pronto!!!')
