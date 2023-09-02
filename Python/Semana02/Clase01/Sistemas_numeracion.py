62e8cd82b59f2282703777a0586006446122f790
Katherinelop <69828273+Katherinelop@users.noreply.github.com>
2023-09-02 14:40:35 -0300

    Add files via upload

---
+++ Python/Semana02/Clase01/Sistemas_numeracion.py
@@ -0,0 +1,25 @@
+# Profundizando en los sistemas de numeración
+
+#Sistema decimal
+a = 10
+print(f"a decimal: {a}")
+
+#Sistema binario
+a = 0b1010
+print(f"a binario : {a}")
+
+#Sistema octal
+a = 0o12
+print(f"a octal: {a}")
+
+#Sistema hexadecimal
+a =0xA
+print(f"a hexadecimal: {a}")
+
+#Base Decimal
+a = int('23', 10)
+print(f"a = base decimal: {a}")
+
+#Base Binario
+a = int('1010', 2)
+print(f"a = base binario: {a}")
