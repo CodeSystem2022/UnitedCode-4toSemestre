cf9e137a6862aa4583a4481e7a8a68015b4f9bdf
Walter Moya <wmmoya@gmail.com>
2023-09-04 11:30:54 -0300

    Video 1 y 4
    2.3 Avanzamos con el modal del carrito en el header -> parte 2 continúa nuestro colaborador (VIDEO 4) JAVASCRIPT JavaScript
    
    2.1 Código de botón de compra (VIDEO 1) JAVASCRIPT Python

---
+++ JavaScript/Semana2/e-commerce2022/client/js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        id:1,
+        productName:"Banana",
+        price:480,
+        quantly:1,
+        img:"/client/media/banana.png"
+    },
+    {
+        id:2,
+        productName:"Leche",
+        price:950,
+        quantly:1,
+        img:"/client/media/leche.png"
+    },
+    {
+        id:3,
+        productName:"Pollo",
+        price:750,
+        quantly:1,
+        img:"/client/media/pollo.png"
+    },
+    {
+        id:4,
+        productName:"Mayonesa",
+        price:510,
+        quantly:1,
+        img:"/client/media/mayonesa.png"
+    }
+]
