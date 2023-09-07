bc873da8cce45d361210e54387fc9c6a2aac2814
Nicolas Aparicio <nicolas.aparicio23@gmail.com>
2023-09-06 21:17:54 -0300

    3.1 Función flecha con template String
    Video 1

---
+++ Node/Semana3/app2.js
@@ -0,0 +1,4 @@
+const saludar = (nombre) =>{ //Funcion de flecha
+    return `Saludos de ${nombre}`; //usamos back tick's: ` esta ubicado despues de la letra p, espacio y aparece `
+}//esto es igual a: 'Saludos' + nombre;
+console.log(saludar('Ariel'));
