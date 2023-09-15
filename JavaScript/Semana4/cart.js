23d3a1261d293c66dbec2382b8fd4a43dd605232
Walter Moya <wmmoya@gmail.com>
2023-09-15 12:01:08 -0300

    VIDEOS 4,5 y 6
    Videos de Javascript

--- JavaScript/Semana4/cart.js
+++ JavaScript/Semana4/cart.js
@@ -13,8 +13,27 @@ increse.addEventListener('click', () => {
     displayCart();
 }) 
 
+// Video 6
 
+    // modal footer
+    const total = cart.reduce((acc, el) => acc + el.price * el.quanty, 0);
 
+    const modalFooter = document.createElement("div");
+    modalFooter.className = "modal-footer"
+    modalFooter.innerHTML = `
+    <div class="total-price">Total: ${total} $</div>
+    <button class="btn-primary" id="checkout-btn"> ir al pago </button>
+    <div id="button-checkout"></div>
+    `;
+    modalContainer.append(modalFooter);
+
+    //MercadoPago
+    
+    const mercadopago = new MercadoPago("TEST-8681a8c6-218e-47ad-98a8-e9d6c941e1b4", {
+      locale: "es-AR", // The most common 
+    });
+
+    const checkoutButton = modalFooter.querySelector("#checkout-btn");
 
 
 
