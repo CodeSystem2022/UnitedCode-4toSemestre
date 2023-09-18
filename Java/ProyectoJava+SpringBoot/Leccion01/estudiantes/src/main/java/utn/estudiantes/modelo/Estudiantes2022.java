cf4be566112ebf5b6e7acb23f5e8eb81640cbc93
lucianocr21 <88411387+lucianocr21@users.noreply.github.com>
2023-09-18 00:35:46 -0300

    Estudiantes2022.java

---
+++ Java/ProyectoJava+SpringBoot/Leccion01/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiantes2022.java
@@ -0,0 +1,28 @@
+package utn.estudiantes.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity // Entity representa a una tabla de datos
+//boilerplate - Repetitivo
+@Data //Agrega los metodos get y set
+@NoArgsConstructor //Construlle al constructor vacio
+@AllArgsConstructor //Crea un constructor con todos los argumentos (id, nombre, apellido, telefono, etc)
+@ToString
+
+public class Estudiantes2022 {
+    //Primero vamos a agregar atributos
+    @Id //Este id es para que vaya agregando el orden del numero de la llave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEstudiante;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+}
