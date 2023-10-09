9e448b5986c1e5d2256ec47b330d9cfec1c992de
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2023-10-09 18:09:58 -0300

    Clase 08 Node - 8.4 #177

--- Node/semana08/callback.js
+++ Node/semana08/callback.js
@@ -1,12 +1,21 @@
 
-function soyAsincrona(miCallback){
-    setTimeout(function (miCallback){
-        console.log('Hola, soy una función asíncrona');
+function hola(nombre, miCallback){
+    setTimeout(function () {
+        console.log('Hola ' + nombre);
         miCallback();
     }, 1000);
 }
 
+function adios(nombre, otroCallback) {
+    setTimeout(function() {
+        console.log('Adios ', nombre);
+        otroCallback();
+    }, 1000);
+}
+
 console.log('Iniciando el proceso...');
-soyAsincrona(function() {
-    console.log('Terminando el proceso...');
+hola('Carlos', function() {
+    adios('Carlos', function() {
+        console.log('Terminando el proceso...');
+    });
 });
