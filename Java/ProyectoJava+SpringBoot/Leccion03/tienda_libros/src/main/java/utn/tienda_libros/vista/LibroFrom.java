fcdd9c676adf2362a1be5bb016cecb1a1d0a2a44
lucianocr21 <88411387+lucianocr21@users.noreply.github.com>
2023-10-25 23:29:53 -0300

    Update LibroFrom.java

--- Java/ProyectoJava+SpringBoot/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Java/ProyectoJava+SpringBoot/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -2,6 +2,7 @@ package utn.tienda_libros.vista;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import utn.tienda_libros.modelo.Libro;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
@@ -13,12 +14,20 @@ public class LibroFrom extends JFrame {
     LibroServicio libroServicio;
     private JPanel panel;
     private JTable tablalibros;
+    private JTextField libroTexto;
+    private JTextField autorTexto;
+    private JTextField precioTexto;
+    private JTextField existenciasTexto;
+    private JButton agregarButton;
+    private JButton modificarButton;
+    private JButton eliminarButton;
     private DefaultTableModel tablaModeloLibros;
 
     @Autowired
-    public  LibroFrom(LibroServicio libroServicio){
+    public  LibroFrom(LibroServicio libroServicio) {
         this.libroServicio = libroServicio;
         iniciarForma();
+        agregarButton.addActionListener(e -> agregarLibro());
     }
 
     private void  iniciarForma(){
@@ -33,6 +42,42 @@ public class LibroFrom extends JFrame {
         int y = (tamanioPantalla.height - getHeight()/2);
         setLocation(x, y);
 
+
+    }
+
+    private void agregarLibro(){
+        //Leer los valores del formulario
+        if(libroTexto.getText().equals("")){
+            mostrarMensaje("Ingrese el nombre del libro");
+            libroTexto.requestFocusInWindow();
+            return;
+        }
+        var nombreLibro = libroTexto.getText();
+        var autor = autorTexto.getText();
+        var precio = Double.parseDouble(precioTexto.getText());
+        var existencias = Integer.parseInt(existenciasTexto.getText());
+        //Creamos el objeto libro
+        var libro = new Libro(null, nombreLibro, autor, precio, existencias);
+        //libro.setNombreLibro(nombreLibro) ;
+        //libro.setAutor(autor);
+        //libro.setPrecio(precio);
+        //libro.setExistencias(existencias);
+        this.libroServicio.guardarLibro(libro);
+        mostrarMensaje("Se agrego el libro...");
+        limpiarFormulario();
+        listarLibros();
+    }
+
+    private void limpiarFormulario(){
+        libroTexto.setText("");
+        autorTexto.setText("");
+        precioTexto.setText("");
+        existenciasTexto.setText("");
+
+    }
+
+    private void mostrarMensaje(String mensaje){
+        JOptionPane.showMessageDialog(this, mensaje);
 
     }
 
@@ -53,7 +98,7 @@ public class LibroFrom extends JFrame {
         //Obtener los libros de la Base de Datos
         var libros = libroServicio.listarLibros();
         //Iteramos cada libro
-        libros.forEach(libro -> {//Funcion lambda
+        libros.forEach((libro) -> {//Funcion lambda
             //Creamos cada registro para agrearlos a la tabla
             Object [] renglonLibro = {
                     libro.getIdLibro(),
@@ -66,4 +111,5 @@ public class LibroFrom extends JFrame {
 
     }
 }
+
 
